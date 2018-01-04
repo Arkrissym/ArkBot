08b6c2b493f3b6d584a521575347b284968b3fa9
Arkrissym <trashmh5@gmail.com>
2018-01-04 17:17:09 +0100

    read token from token.txt add token.txt to .gitignore

---
+++ bot.py
@@ -0,0 +1,38 @@
+import discord
+import asyncio
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!test'):
+        counter = 0
+        tmp = await client.send_message(message.channel, 'Calculating messages...')
+        async for log in client.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
+
+        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+    elif message.content.startswith('!sleep'):
+        await asyncio.sleep(5)
+        await client.send_message(message.channel, 'Done sleeping')
+
+#read the token from token.txt
+tokenFile=open("token.txt", "r")
+token=tokenFile.readline()
+tokenFile.close()
+
+#remove \n at the end of the line
+token=token[:-1]
+
+client.run(token)
