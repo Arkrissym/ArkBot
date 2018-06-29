a6c1a6365a3fb49cc44e39d432fee2d6e9f25b56
Arkrissym <trashmh5@gmail.com>
2018-06-29 17:38:30 +0200

    add option to download audio used by custom commands

--- extensions/customCommands.py
+++ extensions/customCommands.py
@@ -31,6 +31,8 @@ import psycopg2
 from psycopg2 import sql
 import youtube_dl
 import asyncio
+import subprocess
+import pathlib
 
 if not discord.opus.is_loaded():
 	try:
@@ -45,6 +47,45 @@ class CustomCommands:
 	def __init__(self, bot):
 		self.bot=bot
 		self.commands={}
+
+		if config.config["CustomCommands"]["download_audio"].lower() == "true":
+			for g in self.bot.guilds:
+				all_commands=getCommands(g.id)
+				for commmand in all_commands.keys():
+					if all_commands[command]["type"] == "music" and not "{}.mp3".format(command) in os.listdir('{}/../sounds/customCommands/{}'.format(os.path.dirname(__file__), str(g.id))):
+						link_or_name=all_commands[command]["result"]
+						
+						ytdl_opts={
+							'format': 'webm[abr>0]/bestaudio/best',
+							'prefer_ffmpeg': True,
+							'default_search': 'ytsearch',
+							'noplaylist': True,
+							'quiet': True,
+							'ignoreerrors' : True,
+							'logger': log
+						}
+
+						ytdl=youtube_dl.YoutubeDL(ytdl_opts)
+						info=ytdl.extract_info(link_or_name, False)
+
+						if "entries" in info:
+							info=info['entries'][0]
+
+#						if "is_live" in info and info["is_live"] == True:
+#							return
+
+						source=info['url']
+						duration=info['duration']
+		
+						filename='{}/../sounds/customCommands/{}/{}.mp3'.format(os.path.dirname(__file__), str(g.id), command)
+
+						log.info("Downloading audio for command: " + command)
+						pathlib.Path(os.path.dirname(filename)).mkdir(parents=True, exist_ok=True)
+		
+						subprocess.call(["ffmpeg", "-y", "-i" , source, filename, "-loglevel", "warning"])
+						with open('{}/../sounds/customCommands/{}/{}_duration.txt'.format(os.path.dirname(__file__), str(g.id), command), "w") as file:
+							file.write(str(duration) + "\n")
+							file.close()
 
 	def getCommands(self, guild_id):
 		if str(guild_id) in self.commands.keys():
@@ -130,24 +171,33 @@ class CustomCommands:
 					if message.author.voice.channel == None:
 						continue
 
-					ytdl_opts={
-						'format': 'webm[abr>0]/bestaudio/best',
-						'prefer_ffmpeg': True,
-						'default_search': 'ytsearch',
-						'noplaylist': True,
-						'quiet': True,
-						'ignoreerrors' : True,
-						'logger': log
-					}
+					if "{}.mp3".format(command) in os.listdir('{}/../sounds/customCommands/{}'.format(os.path.dirname(__file__), str(message.guild.id))):
+						source='{}/../sounds/customCommands/{}/{}.mp3'.format(os.path.dirname(__file__), str(message.guild.id), command)
+						try:
+							with open('{}/../sounds/customCommands/{}/{}_duration.txt'.format(os.path.dirname(__file__), str(message.guild.id), command), "r") as file:
+								duration=int(file.readline())
+								file.close()
+						except:
+							duration=10
+					else:
+						ytdl_opts={
+							'format': 'webm[abr>0]/bestaudio/best',
+							'prefer_ffmpeg': True,
+							'default_search': 'ytsearch',
+							'noplaylist': True,
+							'quiet': True,
+							'ignoreerrors' : True,
+							'logger': log
+						}
 
-					ytdl=youtube_dl.YoutubeDL(ytdl_opts)
-					info=await self.bot.loop.run_in_executor(None, ytdl.extract_info, all_commands[command]["result"], False)
-					if "entries" in info:
-						info=info['entries'][0]
+						ytdl=youtube_dl.YoutubeDL(ytdl_opts)
+						info=await self.bot.loop.run_in_executor(None, ytdl.extract_info, all_commands[command]["result"], False)
+						if "entries" in info:
+							info=info['entries'][0]
 
-					url=info['url']
-					duration=info['duration']
-#					print(duration)
+						source=info['url']
+						duration=info['duration']
+#						print(duration)
 
 					voice_client=None
 					try:
@@ -155,7 +205,7 @@ class CustomCommands:
 					except discord.ClientException:
 						await message.channel.send(config.strings[config.getLocale(message.channel.guild.id)]['customCommands']['join_channel'])
 					else:
-						voice_client.play(discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(url, options='-loglevel warning'), volume=1.0))
+						voice_client.play(discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(source, options='-loglevel warning'), volume=0.8))
 						await asyncio.sleep(duration+1)
 
 					if voice_client:
@@ -189,6 +239,40 @@ class CustomCommands:
 		if command in self.getCommands(ctx.guild.id).keys():
 			await ctx.send(config.strings[config.getLocale(ctx.guild.id)]['customCommands']['command_exists'].format(command))
 			return
+
+		if config.config["CustomCommands"]["download_audio"].lower() == "true":
+			ytdl_opts={
+				'format': 'webm[abr>0]/bestaudio/best',
+				'prefer_ffmpeg': True,
+				'default_search': 'ytsearch',
+				'noplaylist': True,
+				'quiet': True,
+				'ignoreerrors' : True,
+				'logger': log
+			}
+
+			ytdl=youtube_dl.YoutubeDL(ytdl_opts)
+			info=await self.bot.loop.run_in_executor(None, ytdl.extract_info, link_or_name, False)
+
+			if "entries" in info:
+				info=info['entries'][0]
+
+			if "is_live" in info and info["is_live"] == True:
+				await ctx.send(config.strings["customCommands"]["warn_live"])
+				return
+
+			source=info['url']
+			duration=info['duration']
+		
+			filename='{}/../sounds/customCommands/{}/{}.mp3'.format(os.path.dirname(__file__), str(ctx.guild.id), command)
+
+			log.info("Downloading audio for command: " + command)
+			pathlib.Path(os.path.dirname(filename)).mkdir(parents=True, exist_ok=True)
+		
+			await ctx.bot.loop.run_in_executor(None, subprocess.call, ["ffmpeg", "-y", "-i" , source, filename, "-loglevel", "warning"])
+			with open('{}/../sounds/customCommands/{}/{}_duration.txt'.format(os.path.dirname(__file__), str(ctx.guild.id), command), "w") as file:
+				file.write(str(duration) + "\n")
+				file.close()
 
 		await ctx.bot.loop.run_in_executor(None, self.addCommand, ctx.guild.id, command, "music", link_or_name)
 		await ctx.send(config.strings[config.getLocale(ctx.guild.id)]['customCommands']['add_command'].format(command))
